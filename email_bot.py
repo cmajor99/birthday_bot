dc5b14a971cac1d1c294cab698cb0c09c9f7ae66
cmajor99 <43010847+cmajor99@users.noreply.github.com>
2019-12-04 14:21:40 -0500

    Create email_bot.py

---
+++ email_bot.py
@@ -0,0 +1,17 @@
+import smtplib
+
+choice = input('What is your email address?\n')
+
+content = 'You have X friends who have birthdays today. They are InsertNameVariableHere'
+
+mail = smtplib.SMTP('smtp.gmail.com',587)
+
+mail.ehlo()
+
+mail.starttls()
+
+mail.login('pythongroupproject19@gmail.com','Python1234')
+
+mail.sendmail('pythongroupproject@gmail.com',choice,content)
+
+mail.close()
