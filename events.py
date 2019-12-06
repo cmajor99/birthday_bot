ed2b8455137a90393c04d90601b581e1be28611c
Joël Léger <legerjoe32@gmail.com>
2019-12-06 16:13:44 -0500

    Mistakes

--- events.py
+++ events.py
@@ -1,14 +1,10 @@
 import urllib3,facebook,requests
-
 token= 'EAAGBYgZA1RQcBAMlqCglGOpsMDKxjWlpTGQSq5jZB1NfSUZAW3MXPmVqRRI6zaPDO8jiBMYosrd0aZAQQXMniRp6HCGVSJq2KpSLNlZCaNAr09rWUtxZAaVze794IGZCw6aBL4M8ENrf6Mtbfqtboh6PWWoW3DuO9z9vnbZA3iKc0UDWTvpfc0jtZBpoyLkHhjlDVa6MgAkfGdlTDMZAKELMBTdXb8ltL76ArGYSMkp3BUmwZDZD'
 graph = facebook.GraphAPI(access_token=token, version = 2.8)
 
-#args = {'fields' : 'birthday,name' }
-#friends = graph.get_object("me/friends",**args)
-
-#print(friends)
-
 from facebook import GraphAPI
-friends =[]
-friends = graph.get_object("me?fields=birthday") 
-print(friends)
+
+#profile = graph.get_all_connections(id='me', connection_name='friends')
+post = graph.get_object(id='post_id',fields='message')
+
+print(post['message'])
