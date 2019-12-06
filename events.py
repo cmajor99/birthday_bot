c952354c9bf14175e408288753176e81cee127cf
Joël Léger <legerjoe32@gmail.com>
2019-12-06 15:45:01 -0500

    What am I even doing

---
+++ events.py
@@ -0,0 +1,14 @@
+import urllib3,facebook,requests
+
+token= 'EAAGBYgZA1RQcBAMlqCglGOpsMDKxjWlpTGQSq5jZB1NfSUZAW3MXPmVqRRI6zaPDO8jiBMYosrd0aZAQQXMniRp6HCGVSJq2KpSLNlZCaNAr09rWUtxZAaVze794IGZCw6aBL4M8ENrf6Mtbfqtboh6PWWoW3DuO9z9vnbZA3iKc0UDWTvpfc0jtZBpoyLkHhjlDVa6MgAkfGdlTDMZAKELMBTdXb8ltL76ArGYSMkp3BUmwZDZD'
+graph = facebook.GraphAPI(access_token=token, version = 2.8)
+
+#args = {'fields' : 'birthday,name' }
+#friends = graph.get_object("me/friends",**args)
+
+#print(friends)
+
+from facebook import GraphAPI
+friends =[]
+friends = graph.get_object("me?fields=birthday") 
+print(friends)
--- fetcher.py
+++ fetcher.py
@@ -1,13 +1,18 @@
-GraphRequest request = GraphRequest.newMeRequest(
-  accessToken,
-  new GraphRequest.GraphJSONObjectCallback() {
-    @Override
-    public void onCompleted(JSONObject object, GraphResponse response) {
-      // Insert your code here
-    }
-});
+import urllib3
+import facebook
 
-Bundle parameters = new Bundle();
-parameters.putString("fields", "id,name,birthday");
-request.setParameters(parameters);
-request.executeAsync();
+token = 'EAAGBYgZA1RQcBAMlqCglGOpsMDKxjWlpTGQSq5jZB1NfSUZAW3MXPmVqRRI6zaPDO8jiBMYosrd0aZAQQXMniRp6HCGVSJq2KpSLNlZCaNAr09rWUtxZAaVze794IGZCw6aBL4M8ENrf6Mtbfqtboh6PWWoW3DuO9z9vnbZA3iKc0UDWTvpfc0jtZBpoyLkHhjlDVa6MgAkfGdlTDMZAKELMBTdXb8ltL76ArGYSMkp3BUmwZDZD'
+
+graph = facebook.GraphAPI(access_token=token, version=5.0)
+posts = graph.request('/search?q=Birthday&type=post&limit=10000')
+
+postList = posts['data']
+
+postid = postList[1]['id']
+
+post1 = graph.get_object(id=postid,
+    fields='first_name,last_name,birthday')
+firstName = post1['first_name']
+lastName = post1['last_name']
+birthday = post1['birthday']
+
